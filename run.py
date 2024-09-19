a2eebd1f4d6e510b92673118866b45f23e4172f6
aimanh04 h <aimanhawaemanoh04@gmail.com>
2024-09-19 09:46:43 +0000

    Add all the neccesary functions to the game and added docstrings to describe each functions

--- run.py
+++ run.py
@@ -2,3 +2,63 @@ import random
 import american_countries
 import asian_countries
 import european_countries
+
+def game_menu():
+    """
+    Function to display when you first run the program it display game menu screen.
+    The menu has the options to display instructions or play the game by choosing continent.
+    """
+    pass
+
+def continent():
+    """
+    Function to manage the selection of continent by user to play the game.
+    This function presents a list of continents for the player to choose from and 
+    filter countries or other elements by the chosen continent.
+    """
+    pass
+
+def generate_country():
+    """
+    Function to generate a random country from the chosen continent by user.
+    The function randomly chooses a country from the continent files that the player has to guess in the game.
+    """
+    pass
+
+def guess_letter():
+    """
+    Prompts the player to guess a letter as part of a country generated in the game.
+    This function ensures the player inputs a valid single character.
+    """
+    pass
+
+def validate_letter():
+    """
+    Validates whether the guessed letter is present in the secret country.
+    This function checks if the guessed letter exists in the current country 
+    being guessed and make sures the user entered a singular letter and no other character.
+    """
+    pass
+
+def get_back_menu():
+    """
+    Function to redirect user back to game menu.
+    """
+    pass
+
+def play_again():
+    """
+    Asks the player if they want to play another round after the game ends.
+    This function prompts the player to choose whether to restart the game or quit 
+    after winning or losing.
+    """
+    pass
+
+def main():
+    """
+    The main function that runs the overall game.
+    This function calls all other necessary functions such as the game menu, continent
+    selection, country generation, letter guessing, and checking for win/loss conditions.
+    It keeps the game running in a loop until the player chooses to quit.
+    """
+    pass
